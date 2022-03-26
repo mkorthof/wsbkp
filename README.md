1ff91da2c34ea8080e6f12ec6588acd10c8b0008
Marius <m@mari.us.com>
2022-03-26 20:21:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,29 +1,122 @@
 # wsbkp
-## Wake/Sleep Backup - 'wake up' drive, rsync, poweroff
 
-### Usage
-```
-USAGE: wsbkp.sh -[f|h|w|s|m|o|p|u]|<dirs to backup>
-        [-p]|[-o] power on/off drive
-        [-w]|[-s] wakeup/sleep drive
-        [-m]|[-u] mount/umount drive
-        [-l] list drive, [-f] force
-        <dirs to backup> overwrites var in script
+<u>W</u>ake/<u>S</u>leep <u>B</u>ac<u>k</u>u<u>p</u> -- 'wakes up' usb drive, rsyncs and powers off
+
+A wrapper script to create cheap and easy (partially) 'offline' backups, at file level. Having the disk online only while backing up could offer benefits like data isolation and (some) protection against crypto lockers. The script is meant to run on Linux and uses rsync, hdparm and udisksctl.
+
+This is what the output looks like after starting:
+
 ```
 
-### Example
+root@host:~# wsbkp.sh
 
-BKP_DIRS="/etc/ /root/ /home/ /opt/ /usr/local/bin/"
+* [2020-01-01 17:58:28] Using backup drive "/dev/sdb5" (UUID=abc12345-1234-1234-1234-aaaabbbbcccc)
 
-### Notes
+* Backing up using "rsync -aA --info=flist0,name0,progress2,stats2 --progress --stats"...
+* Dir(s): "/"
+* Target drive: "/dev/sdb5" mounted on "/mnt/bkp" (UUID=abc12345-1234-1234-1234-aaaabbbbcccc)
+* Exclude:
+  /proc/
+  /sys/
+  /lost+found/
+  /mnt/
+  /run/
+  /tmp/
 
-- hdparm: standby timeout: -S 120 (10min), check: -C
-- smart: `smartctl -i -n standby`
-- time: "time rsync <...>" for summary (bashism)
-- rsync: -i itemize changes, -v erbose, -q uiet, -n dryrun
-- info: `udiskctl info -b /dev/sdX`, `udevadm info -p /sys/block/sdX`
-- rm, add:
-   - `udiskctl poweroff`, bind/unbind ?hci-pci
-   - `echo 1 > /sys/block/sdX/device/delete`, `echo 1 > sdX/device/rescan`
-- rescan: `rescan-scsi-bus.sh`
+[2022-03-26 17:58:28] SOURCE DIR: "/" DESTINATION: "/mnt/bkp/"
 
+* Listing "/mnt/bkp" after sync...
+total 100
+drwxr-xr-x  26 root root  4096 Mar 11 06:11 .
+drwxr-xr-x  14 root root  4096 Apr 26  2021 ..
+drwxrwxr-x   2 root root  4096 Jan 28 06:54 bin
+drwxr-xr-x   4 root root  4096 Mar 11 06:15 boot
+drwxr-xr-x  17 root root  4096 Mar 26 17:29 dev
+drwxr-xr-x 194 root root 12288 Mar 26 14:30 etc
+<-- CUT -->
+
+* [2021-0-01 18:24:57] Done. Powering off
+
+```
+
+## Usage
+
+```
+
+Wake/Sleep Backup
+-----------------
+
+SYNOPSIS: wake up usb drive, rsync, power off
+
+USAGE: wsbkp.sh -[o|p][w|s][m|u][l][f] [dirs to backup]
+
+OPTIONS: [-p]|[-o] power on | power off
+         [-w]|[-s] wakeup | sleep
+         [-m]|[-u] mount | umount
+         [-l] list drive info
+         [-f] force
+         [dirs to backup] overwrites setting in script
+
+```
+
+## Installation
+
+Make sure rsync, hdparm and udisksctl (udisks2) are available.
+
+The script can be placed in any location, e.g. /usr/local/sbin (recommended).
+
+## Running
+
+### Backup
+
+Run as root. No command line arguments should be needed, all options are set inside script.
+
+The options listed under 'Usage' are meant in case if e.g. the drive fails to power off automatically after the backup is finished. If that happens, retry manually with `wsbpk -o`.
+
+Another option is to do a one time backup of a different source directory than configured:
+
+`wsbkp /home/user/important /root/more_stuff`
+
+### Restore
+
+To restore one of more files first run `wsbkp -p` to power on the drive and `wsbkp -m` to mount it.
+
+## Configuration
+
+1\) Get UUID of backup drive:
+
+```
+root@host:~# blkid
+/dev/sdb1: UUID="bc12345-1234-1234-1234-aaaabbbbcccc" BLOCK_SIZE="1024" TYPE="ext4" PARTUUID="ab1c2345-01"
+```
+
+Now running `wsbkp.sh -l` should display drive details.
+
+2\) Change settings inside script:
+
+```
+DST_UUID="abc12345-1234-1234-1234-aaaabbbbcccc" # uuid of backup device
+DST_MNT="/mnt/bkp"                              # dst/target path
+BKP_DIRS="/"                                    # dirs to backup
+EXCL_DIRS="
+  --exclude=/proc/
+  --exclude=/sys/
+  --exclude=/lost+found/
+  --exclude=/mnt/
+  --exclude=/run/
+  --exclude=/tmp/
+"
+```
+
+Make sure DST_UUID is set to the correct UUID.
+
+The default is to backup the whole fileystem ('/') and exclude some dirs. It's also possible to specify one or more dirs to backup. Example: `BKP_DIRS="/etc/ /root/ /home/ /opt/ /usr/local/bin/"`
+
+3\) Test and schedule:
+
+Run the script manually to verify correct settings. In case of issues set `LOG=/dev/stdout` for more verbose output from rsync.
+
+If all is well, schedule the script to run for example every night at 3 o'clock
+
+- Cron: `0 3 * * 0 /usr/local/sbin/wsbkp.sh >/dev/null 2>&1`
+- Systemd: see https://www.freedesktop.org/software/systemd/man/systemd.timer.html
--- wsbkp.sh
+++ wsbkp.sh
@@ -1,87 +1,107 @@
 #!/bin/sh
 
-# Wake/Sleep Backup - 'wake up' drive, rsync, poweroff
+################################################################################
+# Wake/Sleep Backup - 'wakes up' usb drive, rsyncs and powers off              #
+################################################################################
 
-# NOTES:
-#   hdparm:  -S 120 sets standby timeout (10min), to check: -C
-#   smart:   'smartctl -i -n standby'
-#   time:    'time rsync <...>' for summary (bashism)
-#   rsync:   [-i] itemize changes [-v] erbose [-q] uiet [-n] dryrun
-#   info:    udiskctl info -b /dev/sdX, udevadm info -p /sys/block/sdX
-#   rm, add: udiskctl poweroff, bind/unbind ?hci-pci
-#            echo 1 > /sys/block/sdX/device/delete, echo 1 > sdX/device/rescan
-#            see http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux
-#   rescan:  rescan-scsi-bus.sh
-
-# EXAMPLE:
+# Example:
+#   DST_UUID="abc12345-1234-1234-1234-aaaabbbbcccc" (see 'blkid' command)
 #   BKP_DIRS="/etc/ /root/ /home/ /opt/ /usr/local/bin/"
 
-BKP_DIRS="/"
+DST_UUID="68bbc7e0-437a-4893-8e62-6b02ef17a857" # uuid of backup device
+DST_MNT="/mnt/bkp"                              # dst/target path
+BKP_DIRS="/"                                    # dirs to backup
+EXCL_DIRS="
+  --exclude=/proc/
+  --exclude=/sys/
+  --exclude=/lost+found/
+  --exclude=/mnt/
+  --exclude=/run/
+  --exclude=/tmp/
+"
 RSYNC="rsync -aA --info=flist0,name0,progress2,stats2 --progress --stats"
-DST_UUID="454e54b4-44d9-4f28-acd9-1f302a0d8c8c" # dst dev uuid (backup drive)
-DST_MNT="/mnt/bkp"
-EXCL="--exclude=/proc/ --exclude=/sys/ --exclude=/jail/ --exclude=/lost+found/ --exclude=/mnt/ --exclude=/run/ --exclude=/tmp/"
 LOG="/var/log/wsbkp.log"
 OUT="/dev/null" # stdout
-FORCE=0 # force even if dev not found
+FORCE=0         # force even if dev not found
 
-#func_dev() {
-#  DST_DEV="$( blkid | grep "$DST_UUID" | cut -d: -f1 )"; \
-#  if [ "$DST_DEV" = "" ] && [ "$FORCE" -eq 0 ]; then
-#    echo "ERROR: Device \"$DST_UUID\" not found, exiting..."; exit 1
-#  fi
-#}
+# END OF CONFIG ################################################################
 
 # Helper function to check if device exists before sleep/mount/list
 func_dev() {
-  DST_DEV="$( blkid -U "$DST_UUID" )" || \
-    { if [ "$FORCE" -eq 0 ]; then
-        printf "ERROR: Device \"%s\" not found, exiting...\n" "$DST_UUID"
-        exit 1
-      fi
-    }
+  #  DST_DEV="$( blkid | grep "$DST_UUID" | cut -d: -f1 )"
+  #  if [ "$DST_DEV" = "" ] && [ "$FORCE" -eq 0 ]; then
+  #    echo "! ERROR: Device \"$DST_UUID\" not found, exiting..."
+  #    exit 1
+  #  fi
+  DST_DEV="$( blkid -U "$DST_UUID" )" || { \
+    if [ "$FORCE" -eq 0 ]; then
+      printf "! ERROR: Device \"%s\" not found, exiting...\n" "$DST_UUID"
+      exit 1
+    fi
+  }
 }
 
 func_wake() {
-  { hdparm -C "/dev/disk/by-uuid/$DST_UUID"; } || \
-    { printf "ERROR: Drive with UUID %s did not wake up, exiting...\n" "$DST_UUID"; exit 1; }
+  hdparm -C "/dev/disk/by-uuid/$DST_UUID" || { \
+    printf "! ERROR: Drive with UUID %s did not wake up, exiting...\n" "$DST_UUID"
+    exit 1
+  }
 }
 
 func_sleep() {
-    func_dev && if findmnt -n "$DST_DEV" | grep -q " $DST_DEV"; then
-      printf "WARNING: \"%s\" is mounted, umount it first\n" "$DST_DEV"
-    else
-      hdparm -Y "$DST_DEV"
-    fi
+  func_dev && if findmnt -n "$DST_DEV" | grep -q " $DST_DEV"; then
+    printf "> WARNING: \"%s\" is mounted, umount it first\n" "$DST_DEV"
+  else
+    hdparm -Y "$DST_DEV"
+  fi
 }
 
+# Verify drive and mount
 func_mnt() {
+  # in case var DST_MNT is empty generate it using device name (/mnt/sdx)
   if [ -z "$DST_MNT" ]; then
-    func_dev && { TMP="$( basename "$DST_DEV" )"; if echo "$TMP" | grep -q "^sd[a-z]"; then DST_MNT="/mnt/${TMP}"; fi; }
+    func_dev && { \
+      TMP="$( basename "$DST_DEV" )"
+      if echo "$TMP" | grep -q "^sd[a-z]"; then
+        DST_MNT="/mnt/${TMP}"
+      fi
+    }
   fi
   if [ ! -d "$DST_MNT" ]; then
-    mkdir "$DST_MNT"; func_dev && mount "$DST_DEV" "$DST_MNT" && printf "* INFO: mounted \"%s\" \"%s\"\n" "$DST_DEV" "$DST_MNT"
+    mkdir "$DST_MNT"
   fi
+  func_dev && \
+  mount "$DST_DEV" "$DST_MNT" && \
+  printf -- "- INFO: mounted \"%s\" \"%s\"\n" "$DST_DEV" "$DST_MNT"
 }
 
 func_umnt() { umount "$DST_MNT"; }
 
+
+# Power on using usb host controller interface (xchi, ehci, ohci)
 func_power_on() {
   #shopt -s nullglob
   for i in /sys/bus/pci/drivers/?hci-pci ; do
-    if ! cd "$i" ; then printf "ERROR: Failed to change directory to %s\n" "$i"; exit 1; fi
-    printf "* INFO: Resetting devices from %s...\n" "$i"
+    cd "$i" || { \
+      printf "! ERROR: Failed to change directory to %s\n" "$i"
+      exit 1
+    }
+    printf -- "- INFO: Resetting devices from %s...\n" "$i"
     for j in ????:??:??.? ; do
-      printf "%s" "$j" > unbind; printf "%s" "$j" > bind; sleep 1
+      printf "%s" "$j" > unbind; printf "%s" "$j" > bind
+      sleep 1
     done
   done
 }
 
 func_power_off() {
   func_dev && {
-    command -v udisksctl >/dev/null 2>&1 && udisksctl power-off --no-user-interaction -b "$DST_DEV" || \
-      echo "WARNING: udiskctl did not run successfully"
-    exit 1
+    command -v udisksctl >/dev/null 2>&1 && { \
+      udisksctl power-off --no-user-interaction -b "$DST_DEV" || { \
+        echo "> WARNING: udiskctl did not run successfully"
+        exit 1
+      }
+    }
   }
 }
 
@@ -90,21 +110,27 @@ func_list () {
   FORCE=1
   func_dev
   printf "\nDST_UUID=\"%s\" DST_DEV=\"%s\"\n\n" "$DST_UUID" "$DST_DEV"
-  echo "fdisk :"; fdisk -l 2>/dev/null|grep "Disk /dev/sd[b-z]"
+  echo "fdisk :"
+  fdisk -l 2>/dev/null|grep "Disk /dev/sd[b-z]"
   echo
-  echo "blkid :"; blkid | grep -Ev "/dev/(sda[0-9]|loop|mapper)|swap|LUKS"
+  echo "blkid :"
+  blkid | grep -Ev "/dev/(sda[0-9]|loop|mapper)|swap|LUKS"
   echo
   echo "findmnt :"
-  findmnt -nr | grep -Ev '/(sda|pts)|^/(proc|run|sys|boot|jail)|tmpfs|overlay|fuseblk|mqueue|hugepages' | grep -E "$DST_DEV|$DST_MNT"
+  findmnt -nr | \
+    grep -Ev '/(sda|pts)|^/(proc|run|sys|boot|jail)|tmpfs|overlay|fuseblk|mqueue|hugepages' | \
+    grep -E "$DST_DEV|$DST_MNT"
   echo
   echo "/sys/devices :"
   find /sys/devices -name block -regex '.*usb.*' -exec sh -c '
-    printf "%s: %s\n" "$1" "$(ls "$1")";
+    printf "%s: %s\n" "$1" "$(ls "$1")"
     printf "%s: %s\n" "$(echo "$1"/../model|sed s@"$1"/../@\ @)" "$(cat "$1"/../model 2>/dev/null )"' sh "{}" \; | \
-      sed -e 's|/sys/devices/pci|/pci|g' -e 's/0000:00[/:]\?//g'; echo
+      sed -e 's|/sys/devices/pci|/pci|g' -e 's/0000:00[/:]\?//g'
+    echo
   echo "/sys/block :"
   for i in /sys/block/sd[b-z]; do
-    printf "%s device/state: %s power/control: %s\n" "$i" "$(cat "$i"/device/state 2>/dev/null)" "$(cat "$i"/power/control 2>/dev/null)"
+    printf "%s device/state: %s power/control: %s\n" \
+      "$i" "$(cat "$i"/device/state 2>/dev/null)" "$(cat "$i"/power/control 2>/dev/null)"
   done
   echo
   echo "/sys/bus/pci/drivers :"
@@ -112,13 +138,17 @@ func_list () {
   echo
 }
 
-func_help () {
-  printf "USAGE: %s -[f|h|w|s|m|o|p|u]|<dirs to backup>\n\t" "$( basename "$0" )"
-  printf "[-p]|[-o] power on/off drive\n\t"
-  printf "[-w]|[-s] wakeup/sleep drive\n\t"
-  printf "[-m]|[-u] mount/umount drive\n\t"
-  printf "[-l] list drive, [-f] force\n\t"
-  printf "<dirs to backup> overwrites var in script\n"
+func_help ()  {
+  printf "\nWake/Sleep Backup\n"
+  printf -- "-----------------\n\n"
+  printf "SYNOPSIS: wake up usb drive, rsync, power off\n\n"
+  printf "USAGE: %s -[o|p][w|s][m|u][l][f] [dirs to backup]\n\n" "$( basename "$0" )"
+  printf "OPTIONS: [-p]|[-o] power on | power off\n"
+  printf "\t [-w]|[-s] wakeup | sleep\n"
+  printf "\t [-m]|[-u] mount | umount\n"
+  printf "\t [-l] list drive info\n"
+  printf "\t [-f] force\n"
+  printf "\t [dirs to backup] overwrites setting in script\n\n"
 }
 
 # handle arguments
@@ -131,56 +161,88 @@ if printf -- "%s" "$*" | grep -q -- '\-h'; then func_help; exit
   elif printf -- "%s" "$*" | grep -q -- '\-o'; then func_power_off; exit
   elif printf -- "%s" "$*" | grep -q -- '\-p'; then func_power_on; exit
   elif printf -- "%s" "$*" | grep -q -- '\-l'; then func_list; exit
-  elif  printf -- "%s" "$*" | grep -q -- '\-'; then func_help; exit
-  elif [ "$*" != "" ]; then BKP_DIRS="$&"
+  elif printf -- "%s" "$*" | grep -q -- '\-'; then func_help; exit
+  elif [ "$*" != "" ]; then BKP_DIRS="$*"
 fi
+
+#
+# NOTES:
+#
+#   hdparm:  -S 120 sets standby timeout (10min), to check: -C
+#   smart:   'smartctl -i -n standby'
+#   time:    'time rsync <...>' for summary (bashism)
+#   rsync:   [-i] itemize changes [-v] erbose [-q] uiet [-n] dryrun
+#   info:    udiskctl info -b /dev/sdX, udevadm info -p /sys/block/sdX
+#   rm, add: udiskctl poweroff, bind/unbind ?hci-pci
+#            echo 1 > /sys/block/sdX/device/delete, echo 1 > sdX/device/rescan
+#            see http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux
+#   rescan:  rescan-scsi-bus.sh
+#
 
 # Main
 echo
-blkid -U "$DST_UUID" || { printf "* [%s] Powering on backup drive (UUID=%s)...\n" "$(date +%F_%T )" "$DST_UUID"; func_power_on; }
+blkid -U "$DST_UUID" >/dev/null || { \
+  printf "* [%s] Powering on backup drive (UUID=%s)...\n" "$(date +%F\ %T )" "$DST_UUID"
+  func_power_on
+}
 func_dev && { printf "* [%s] Using backup drive \"%s\" (UUID=%s)\n" "$(date +%F\ %T )" "$DST_DEV" "$DST_UUID"; }
 if [ ! -d "$DST_MNT" ]; then
-  mkdir "$DST_MNT" || { printf "ERROR: Could not create %s\n" "$DST_MNT"; exit 1; }
+  mkdir "$DST_MNT" || { \
+    printf "! ERROR: Could not create %s\n" "$DST_MNT"
+    exit 1
+  }
 fi
-func_mnt || { printf "ERROR: Could not mount \"%s\" \"%s\", exiting...\n" "$DST_DEV" "$DST_MNT"; exit 1; }; echo
+func_mnt || { \
+  printf "\n! ERROR: Could not mount \"%s\" on \"%s\", exiting...\n" "$DST_DEV" "$DST_MNT"
+  exit 1
+}
+echo
 if [ -d "$DST_MNT" ]; then
   if findmnt -n "$DST_MNT" | grep -q " $DST_DEV"; then
     printf "* Backing up using \"%s\"...\n" "$RSYNC"
     printf "* Dir(s): \"%s\"\n" "$BKP_DIRS"
-    printf "* Target drive: \"%s\" mounted on \"%s\" (%s)\n" "$DST_DEV" "$DST_MNT" "$DST_UUID"
-    printf "* Exclude: %s\n\n" "$( echo -- "$EXCL" | sed 's/--exclude=//g' )"
+    printf "* Target drive: \"%s\" mounted on \"%s\" (UUID=%s)\n" "$DST_DEV" "$DST_MNT" "$DST_UUID"
+    printf "* Exclude: %s\n\n" "$( echo "$EXCL_DIRS" | sed 's/--exclude=//g' )"
     # rsync dirs and poweroff if loop returns 0 (warn if it fails)
     ( 
       for i in $BKP_DIRS; do
         if [ -d "$i" ]; then
           MSG="sleeping 10s... CTRL-C TO ABORT"
           if ! echo "$i" | grep -q '^/'; then
-            printf "* WARNING: \"$i\" has no leading slash, %s\n\n" "$MSG"; sleep 10
+            printf "> WARNING: \"$i\" has no leading slash, %s\n\n" "$MSG"
+            sleep 10
           fi
           if ! echo "$i" | grep -q '/$'; then
-            printf "* WARNING: \"$i\" has no trailing slash, %s\n\n" "$MSG"; sleep 10
+            printf "> WARNING: \"$i\" has no trailing slash, %s\n\n" "$MSG"
+            sleep 10
           fi
-          printf "[%s] SOURCE DIR: \"%s\" DEST: \"%s\"" "$(date +%F\ %T )" "$i" "${DST_MNT}${i}\n" | tee -a "$LOG"
+          printf "* [%s] SOURCE DIR: \"%s\" DESTINATION: \"%s\"\n" "$(date +%F\ %T )" "$i" "${DST_MNT}${i}" | \
+            tee -a "$LOG"
           if [ ! -d "${DST_MNT}${i}" ]; then
-            printf "Creating %s\n" "${DST_MNT}${i}"
-            mkdir -p "${DST_MNT}${i}" || printf "ERROR: Could not create %s\n" "${DST_MNT}${i}"
+            printf "+ Creating %s\n" "${DST_MNT}${i}"
+            mkdir -p "${DST_MNT}${i}" || printf "! ERROR: Could not create %s\n" "${DST_MNT}${i}"
           fi
-          $RSYNC $EXCL "$i" "${DST_MNT}${i}" >"$OUT" 2>&1
+          $RSYNC "$EXCL_DIRS" "$i" "${DST_MNT}${i}" >"$OUT" 2>&1
         else
-          printf "ERROR: \"%s\" does not exist\n" "$i"
+          printf "! ERROR: \"%s\" does not exist\n" "$i"
         fi
         echo
       done
-    ) && {
-      printf "* Listing \"%s\"\n%s\n" "$DST_MNT" "$(ls -la "$DST_MNT")"
-      printf "[%s] Done. Powering off\n" "$(date +%F\ %T )" | tee -a "$LOG"
+    ) && { \
+      printf "* Listing \"%s\" after sync\n%s\n\n" "$DST_MNT" "$(ls -la "$DST_MNT")"
+      printf "* [%s] Done. Powering off\n" "$(date +%F\ %T )" | tee -a "$LOG"
       sleep 30 && func_umnt && func_power_off;
-    } || { printf"[%s] WARNING: Check if drive is unmounted and powered down\n" "$(date +%F\ %T )" | tee -a "$LOG"; }
+    } || { \
+      printf"> [%s] WARNING: Check if drive is unmounted and powered down\n" "$(date +%F\ %T )" | \
+        tee -a "$LOG"
+    }
   else
-    printf "ERROR: mount \"%s\" not found, exiting\n" "$DST_MNT"; exit 1
+    printf "ERROR: mount \"%s\" not found, exiting\n" "$DST_MNT"
+    exit 1
   fi
 else
-  printf "ERROR: \"%s\" does not exists, exiting...\n" "$DST_MNT"; exit 1
+  printf "ERROR: \"%s\" does not exists, exiting...\n" "$DST_MNT"
+  exit 1
 fi
 echo
 if [ -d "$DST_MNT" ]; then
